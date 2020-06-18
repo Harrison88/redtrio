679f851803e9d1e04aa9164274022d60df649a86
Harrison Morgan <harrison.morgan@gmail.com>
2020-06-18 11:38:11 -0500

    Increased test coverage of client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,27 @@
+import pytest
+import trio
 from pytest_trio.enable_trio_mode import *
+
+
+async def fake_server(stream, *, task_status=trio.TASK_STATUS_IGNORED):
+    task_status.started()
+    while True:
+        data = await stream.receive_some()
+        if data == b"*1\r\n$4\r\nPING\r\n":
+            await stream.send_all(b"+PONG\r\n")
+
+
+@pytest.fixture
+async def trickle_connection(nursery):
+    server_socket, client_socket = trio.testing.memory_stream_pair()
+    await nursery.start(fake_server, server_socket)
+
+    async def trickle():
+        while trio.testing.memory_stream_pump(
+            server_socket.send_stream, client_socket.receive_stream, max_bytes=1
+        ):
+            await trio.sleep(1)
+
+    server_socket.send_stream.send_all_hook = trickle
+
+    return client_socket
--- tests/lowlevel/test_client.py
+++ tests/lowlevel/test_client.py
@@ -1,7 +1,10 @@
+from functools import partial
+
 import pytest
 import trio
 
 from redtrio.lowlevel import RedisClient
+from redtrio.lowlevel import connections
 
 
 @pytest.fixture
@@ -14,3 +17,15 @@ async def test_call_hello(client):
     result = await client.call(b"HELLO", b"3")
     assert result[b"proto"] == 3
     assert len(result) == 7
+
+
+def test_use_connection_pool():
+    pool = connections.ConnectionPool(host="127.0.0.1", port=6379)
+    client = RedisClient(connection_pool=pool)
+    assert client.connection_pool is pool
+
+
+async def test_parse_streamed_data(autojump_clock, nursery, client, trickle_connection):
+    client.connection_pool.pool.append(trickle_connection)
+    result = await client.call(b"PING")
+    assert result == "PONG"
