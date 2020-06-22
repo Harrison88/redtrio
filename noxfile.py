798a2c95f457effa116d13872ee02c578a8536ac
Harrison Morgan <harrison.morgan@gmail.com>
2020-06-21 21:14:33 -0500

    Add flake8 linting as described by Hypermodern Python

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,61 @@
+import tempfile
+
 import nox
 
 
 @nox.session(python=["3.8"])
 def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-trio"
+    )
+    session.run("pytest", *args)
+
+
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8"])
+def lint(session):
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-docstrings",
+        "flake8-import-order",
+        "darglint",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
