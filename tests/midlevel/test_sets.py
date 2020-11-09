b45cef5e9b41953056bbf1f7c6003d67243b33c1
Harrison Morgan <harrison.morgan@gmail.com>
2020-11-09 12:02:58 -0600

    Write tests for SADD.

---
+++ tests/midlevel/test_sets.py
@@ -0,0 +1,41 @@
+"""This module contains the tests for Redis' sets commands."""
+
+import pytest
+
+from redtrio.midlevel import MidlevelClient
+
+
+@pytest.fixture
+async def client():
+    """A fresh client for every test.
+
+    Also flushes the database to prevent conflicts.
+
+    Returns:
+        An instance of :class:`midlevel.MidlevelClient`.
+    """
+    client = MidlevelClient()
+    await client.call("FLUSHALL")
+    return client
+
+
+async def test_sadd(client):
+    """It returns the proper responses for SADD."""
+    key = "midlevel_sadd_test"
+    value = "Hello, Sets!"
+
+    # SADD returns the number of elements added to the set, not including any
+    # elements which were already in the set.
+    expected = 1
+    actual = await client.sadd(key, value)
+    assert actual == expected
+
+    # Adding the same value again should return 0, since it's already in the set.
+    expected = 0
+    actual = await client.sadd(key, value)
+    assert actual == expected
+
+    # Adding two new values should return 2.
+    expected = 2
+    actual = await client.sadd(key, "new", "also new")
+    assert actual == expected
